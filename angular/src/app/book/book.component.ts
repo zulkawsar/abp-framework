6f40c171e0bc5bf0a15c101a1da4427adbee86d4
zulkawsar <zulkawsar@gmail.com>
2024-01-29 20:35:00 +0600

    ➕ Add book view
    Type(s): ADD
    
    Issue(s):
    - JIRA: 1

--- angular/src/app/book/book.component.ts
+++ angular/src/app/book/book.component.ts
@@ -1,5 +1,10 @@
 import { ListService, PagedResultDto } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbDateAdapter, NgbDateNativeAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { bookTypeOptions } from '@proxy/books';
+
+import { Confirmation, ConfirmationService } from '@abp/ng.theme.shared';
 import { BookDto } from '@proxy/dto/books';
 import { BookService } from '@proxy/services';
 
@@ -7,18 +12,82 @@ import { BookService } from '@proxy/services';
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss'],
-  providers: [ListService],
+  providers: [ListService, { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter }],
 })
 export class BookComponent implements OnInit {
   book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
 
-  constructor(public readonly list: ListService, private bookService: BookService) {}
+  selectedBook = {} as BookDto; // declare selectedBook
+
+  form: FormGroup;
+
+  bookTypes = bookTypeOptions;
+
+  isModalOpen = false;
+
+  constructor(
+    public readonly list: ListService,
+    private bookService: BookService,
+    private fb: FormBuilder,
+    private confirmation: ConfirmationService
+  ) {}
 
   ngOnInit() {
     const bookStreamCreator = query => this.bookService.getList(query);
 
     this.list.hookToQuery(bookStreamCreator).subscribe(response => {
       this.book = response;
+    });
+  }
+
+  createBook() {
+    this.selectedBook = {} as BookDto; // reset the selected book
+    this.buildForm();
+    this.isModalOpen = true;
+  }
+
+  // Add editBook method
+  editBook(id: string) {
+    this.bookService.get(id).subscribe(book => {
+      this.selectedBook = book;
+      this.buildForm();
+      this.isModalOpen = true;
+    });
+  }
+
+  buildForm() {
+    this.form = this.fb.group({
+      name: [this.selectedBook.name || '', Validators.required],
+      type: [this.selectedBook.type || null, Validators.required],
+      publishDate: [
+        this.selectedBook.publishDate ? new Date(this.selectedBook.publishDate) : null,
+        Validators.required,
+      ],
+      price: [this.selectedBook.price || null, Validators.required],
+    });
+  }
+
+  // change the save method
+  save() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const request = this.selectedBook.id
+      ? this.bookService.update(this.selectedBook.id, this.form.value)
+      : this.bookService.create(this.form.value);
+
+    request.subscribe(() => {
+      this.isModalOpen = false;
+      this.form.reset();
+      this.list.get();
+    });
+  }
+  delete(id: string) {
+    this.confirmation.warn('::AreYouSureToDelete', '::AreYouSure').subscribe(status => {
+      if (status === Confirmation.Status.confirm) {
+        this.bookService.delete(id).subscribe(() => this.list.get());
+      }
     });
   }
 }
--- angular/src/app/book/book.module.ts
+++ angular/src/app/book/book.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap'; // add this line
 import { SharedModule } from '../shared/shared.module';
-
 import { BookRoutingModule } from './book-routing.module';
 import { BookComponent } from './book.component';
 
 @NgModule({
   declarations: [BookComponent],
-  imports: [BookRoutingModule, SharedModule],
+  imports: [
+    BookRoutingModule,
+    SharedModule,
+    NgbDatepickerModule, // add this line
+  ],
 })
 export class BookModule {}
